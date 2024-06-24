8ff97da73054b2394a7afe55895650dd6348f0ad
alenmirc <167504095+alenmirc@users.noreply.github.com>
2024-06-25 02:54:35 +0800

    Update signup.php

--- signup.php
+++ signup.php
@@ -157,7 +157,7 @@ else {
   $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
   $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
   $mail->Username   = 'lfprsofficial@gmail.com';                     //SMTP username
-  $mail->Password   = 'yghwtaanwkizmbat';                               //SMTP password
+  $mail->Password   = '//change!!';                               //SMTP password
   $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
   $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
